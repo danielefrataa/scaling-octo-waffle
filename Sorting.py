84a513cff2171907c41713e6880b7411a12314ae
danielefrataa <106148834+danielefrataa@users.noreply.github.com>
2022-05-24 16:10:23 +0700

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,18 @@
+def bubble_sort(array, desc=False):
+    n = len(array) # jumlah list
+    # perulangan pertama
+    for i in range(n): 
+        # perulangan kedua
+        for j in range(n - i - 1):
+            # bandingkan masing" elemen
+            if desc:
+                if array[j] < array[j + 1]:
+                    # jika lebih kecil, tukar.
+                    array[j], array[j + 1] = array[j + 1], array[j]
+            else:
+                if array[j] > array[j + 1]:
+                    # jika lebih besar, tukar.
+                    array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+unordered = [5, 3, 4, 8, 1, 2, 9, 6]
+print(bubble_sort(unordered))
